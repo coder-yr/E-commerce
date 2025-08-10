a5e9d514bdc640381f841319be544e58a264077b
Yash Ram <yashramt@gmail.com>
2025-08-10 06:16:42 +0000

    i am unable to signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -6,7 +6,6 @@ import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
