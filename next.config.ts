21aea0300b70810fa1af5befdea13d3c79a73ce6
Yash Ram <yashramt@gmail.com>
2025-08-10 06:07:37 +0000

    solve the issue

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    FIREBASE_PROJECT_ID: 'shopsphere-jz759',
+    FIREBASE_APP_ID: '1:70281079862:web:930139378f04cc30033c97',
+    FIREBASE_STORAGE_BUCKET: 'shopsphere-jz759.firebasestorage.app',
+    FIREBASE_API_KEY: 'AIzaSyAtuuAjbL9pKPw45XK8vWBPtRGqgg_PBwM',
+    FIREBASE_AUTH_DOMAIN: 'shopsphere-jz759.firebaseapp.com',
+    FIREBASE_MESSAGING_SENDER_ID: '70281079862',
+  },
 };
 
 export default nextConfig;
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  "projectId": "shopsphere-jz759",
-  "appId": "1:70281079862:web:930139378f04cc30033c97",
-  "storageBucket": "shopsphere-jz759.firebasestorage.app",
-  "apiKey": "AIzaSyAtuuAjbL9pKPw45XK8vWBPtRGqgg_PBwM",
-  "authDomain": "shopsphere-jz759.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "70281079862"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
