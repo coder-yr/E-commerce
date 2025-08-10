e72cb590f2c981c30ccedd8512e68d3dc4fcff97
Yash Ram <yashramt@gmail.com>
2025-08-10 06:13:27 +0000

    still issue persist

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,7 +11,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
 export { app, auth };
